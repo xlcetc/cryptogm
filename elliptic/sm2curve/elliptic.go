afb14de509747d3d8feef6330a36ccfda1088a21
boyxuper <boyxuper@gmail.com>
2022-08-02 08:08:43 +0800

    better golang module interface

--- elliptic/sm2curve/elliptic.go
+++ elliptic/sm2curve/elliptic.go
@@ -1,19 +1,18 @@
 package sm2curve
 
 import (
+	"crypto/ecdsa"
 	"crypto/elliptic"
-	"sync"
 )
 
-var initOnce sync.Once
+type SM2PublicKey ecdsa.PublicKey
 
-func initAll() {
-	initP256()
+func init() {
+	initSM2P256()
 }
 
-// P256 returns a Curve which implements sm2 curve.
+// SM2P256 returns a Curve which implements sm2 curve.
 // The cryptographic operations are implemented using constant-time algorithms.
-func P256() elliptic.Curve {
-	initOnce.Do(initAll)
+func SM2P256() elliptic.Curve {
 	return p256
 }
--- elliptic/sm2curve/sm2p256_amd64.go
+++ elliptic/sm2curve/sm2p256_amd64.go
@@ -38,9 +38,9 @@ var (
 	precomputeOnce  sync.Once
 )
 
-func initP256() {
+func initSM2P256() {
 	// See FIPS 186-3, section D.2.3
-	p256.CurveParams = &elliptic.CurveParams{Name: "P-256"}
+	p256.CurveParams = &elliptic.CurveParams{Name: "SM2-P256"}
 	p256.P, _ = new(big.Int).SetString("FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF", 16)
 	p256.N, _ = new(big.Int).SetString("FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123", 16)
 	p256.B, _ = new(big.Int).SetString("28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93", 16)
